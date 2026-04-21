ad011ef94b42e3c4975a1fc1540100697f7cb3da
YB <younessbarahou028@gmail.com>
2026-04-21 11:51:06 +0100

    d

--- solution.py
+++ solution.py
@@ -14,29 +14,27 @@ class Solution:
             c for c in start_hub.neighbors.values()
             if not c.visited_djikstra
         ]
+        for q in queue:
+            q.precedent = current_node
+            q.total_cost = int(q.cost)
         queue = sorted(queue, key=lambda x: x.total_cost)
         while True:
+            # selecting and poping from the queue
             current_node = queue[0]
+            queue = queue[1:]
+            if current_node.visited_djikstra is True:
+                continue
             current_node.visited_djikstra = True
             if current_node == end_hub:
                 break
-            queue = queue[1:]
             optimal_neighbors: List[Hub] = [
                 c for c in current_node.neighbors.values()
                 if not c.visited_djikstra
             ]
             for o in optimal_neighbors:
                 if o.total_cost > current_node.total_cost + o.cost:
-                    o.total_cost = current_node.total_cost + o.cost
+                    o.total_cost = current_node.total_cost + int(o.cost)
                     o.precedent = current_node
-                # we check for duplicate
-                if o in queue:
-                    clone: List[Hub] = [q for q in queue if q.name == o.name]
-                    # we remove the duplicated element
-                    queue = list(
-                            filter(lambda x: x.name != clone[0].name, queue))
-                    # we add the smallest element
-                    queue.append(min(o, clone[0], key=lambda x: x.total_cost))
-                else:
-                    queue.append(o)
+                queue.append(o)
             queue = sorted(queue, key=lambda x: x.total_cost)
+            path_result: List[Hub] = []
